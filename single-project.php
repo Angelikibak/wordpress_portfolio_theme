0188606206fa1c2b811fd8724aee16d3509ca2e2
Angelikibak <agelikibakogianni@gmail.com>
2026-04-16 15:39:13 +0200

    Update design in single project page

--- single-project.php
+++ single-project.php
@@ -1,23 +1,47 @@
 <?php get_header(); ?>
 
-<main class="site-main single-project">
+<main class="single-project">
+    <div class="single-project__inner">
+        <?php if (have_posts()) : ?>
+            <?php while (have_posts()) : the_post(); ?>
 
-    <?php if (have_posts()) : ?>
-        <?php while (have_posts()) : the_post(); ?>
-            <article class="single-project__article">
+                <?php
+                $project_categories = get_the_terms(get_the_ID(), 'project_category');
+                $project_category_link = '';
+                $project_category_name = '';
 
-                <header class="single-project__header">
-                    <h1 class="single-project__title"><?php the_title(); ?></h1>
-                </header>
+                if ($project_categories && !is_wp_error($project_categories)) {
+                    $primary_category = $project_categories[0];
+                    $project_category_link = get_term_link($primary_category);
+                    $project_category_name = $primary_category->name;
+                }
+                ?>
 
-                <div class="single-project__content">
-                    <?php the_content(); ?>
-                </div>
+                <nav class="single-project__topbar" aria-label="Project navigation">
+                    <a class="single-project__back-link" href="<?php echo esc_url(home_url('/')); ?>">
+                        Back to Home
+                    </a>
 
-            </article>
-        <?php endwhile; ?>
-    <?php endif; ?>
+                    <?php if ($project_category_link && !is_wp_error($project_category_link)) : ?>
+                        <a class="single-project__back-link" href="<?php echo esc_url($project_category_link); ?>">
+                            Back to <?php echo esc_html($project_category_name); ?>
+                        </a>
+                    <?php endif; ?>
+                </nav>
 
+                <article class="single-project__article">
+                    <header class="single-project__header">
+                        <h1 class="single-project__title"><?php the_title(); ?></h1>
+                    </header>
+
+                    <div class="single-project__content">
+                        <?php the_content(); ?>
+                    </div>
+                </article>
+
+            <?php endwhile; ?>
+        <?php endif; ?>
+    </div>
 </main>
 
 <?php get_footer(); ?>
